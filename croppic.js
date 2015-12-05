2d33b1a38e9f38bf42a0992b6eeae175ed7972e4
Ryan Yonzon <rawswift@gmail.com>
2015-12-06 06:15:09 +0800

    Additional POST data: X/Y coordinates, width, and height relative to the original size

--- croppic.js
+++ croppic.js
@@ -621,7 +621,11 @@
 					imgX1:Math.abs( parseInt( that.img.css('left') ) ),
 					cropH:that.objH,
 					cropW:that.objW,
-					rotation:that.actualRotation
+					rotation:that.actualRotation,
+            		imgRelativeY:Math.ceil( that.imgInitH * ( Math.abs( parseInt( that.img.css('top') ) ) / that.imgH) ),
+            		imgRelativeX:Math.ceil( that.imgInitW * ( Math.abs( parseInt( that.img.css('left') ) ) / that.imgW) ),
+					cropRelativeH:( that.objH / that.imgH ) * that.imgInitH,
+					cropRelativeW:( that.objW / that.imgW ) * that.imgInitW
 				};
 			
 			var formData;
